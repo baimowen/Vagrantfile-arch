fdc3607e2e690856ef9766e5ac0a3e80eec6df87
baimowen <bmw2032044306@outlook.com>
2025-06-10 12:34:23 +0800

    modify .zshrc

--- arch_packages.sh
+++ arch_packages.sh
@@ -263,15 +263,17 @@ setopt HIST_IGNORE_SPACE
 
 source <(/usr/bin/fzf --zsh)
 
-# git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh/zsh-syntax-highlighting
-[ -f ~/.zsh/zsh-catppuccin/themes/catppuccin_mocha-zsh-syntax-highlighting.zsh ] && source $_ || :
-source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 # git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-[ -f ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh ] && source $_ || :
+# [ -f ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh ] && source $_ || :
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 # git clone https://github.com/zap-zsh/sudo.git ~/.zsh/zsh-sudo
-[ -f ~/.zsh/zsh-sudo/sudo.plugin.zsh ] && source $_ || :
+# [ -f ~/.zsh/zsh-sudo/sudo.plugin.zsh ] && source $_ || :
+source ~/.zsh/zsh-sudo/sudo.plugin.zsh
 # git clone https://github.com/catppuccin/zsh-syntax-highlighting.git ~/.zsh/zsh-catppuccin
-[ -f ~/.zsh/zsh-catppuccin/themes/catppuccin_mocha-zsh-syntax-highlighting.zsh ] && source $_ || :
+# [ -f ~/.zsh/zsh-catppuccin/themes/catppuccin_mocha-zsh-syntax-highlighting.zsh ] && source $_ || :
+source ~/.zsh/zsh-catppuccin/themes/catppuccin_mocha-zsh-syntax-highlighting.zsh
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh/zsh-syntax-highlighting
+source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # starship
 eval "$(starship init zsh)"
